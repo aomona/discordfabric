530df15ac27977555135cbcec83025e7a329a64a
aomona <yuka41623@gmail.com>
2025-04-24 21:56:06 +0900

    create IntelliJ IDEA project

---
+++ src/main/java/tech/aomona/discordfabric/Discordfabric.java
@@ -0,0 +1,10 @@
+package tech.aomona.discordfabric;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Discordfabric implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
